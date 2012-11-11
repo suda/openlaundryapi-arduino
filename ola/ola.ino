82e755e5043a4c3daa5b47e5d0d16bcc9bda439c
Mateusz Kozak <mateusz@mkozak.pl>
2012-11-11 18:06:11 +0100

    sending data to server

--- ola/ola.ino
+++ ola/ola.ino
@@ -1,63 +1,58 @@
 /*
 
-This is OpenLaundryAPI client.
-
-Connects to wi-fi network and sends 
-json data received from analog input
-
-created 10 November 2012
-by mkozak (github.com/meal) - OpenLaundryAPI team (github.com/OpenLaundryAPI)
-
-*/
+ This is OpenLaundryAPI client.
+ 
+ Connects to wi-fi network and sends 
+ data received from analog input
+ 
+ created 10 November 2012
+ by mkozak (github.com/meal) - OpenLaundryAPI team (github.com/OpenLaundryAPI)
+ 
+ */
 #include <WiFi.h>
 #include <SPI.h>
 
 WiFiClient client;
 
-char ssid[] = "BL_GUEST"; // your network SSID
-char pass[] = "blaipblaip"; // your network password
+char ssid[] = "Video"; // your network SSID
+char pass[] = "blaipvideo"; // your network password
+char device[] = "stara"; // your device ID
+char token[] = "testmatkozak"; // secret auth token
 int status = WL_IDLE_STATUS;
 const int analogInPin = A0;
 const int analogOutPin = 9;
 char serverName[] = "openlaundryapi.org";
-byte mac[6];
 int sensorValue = 0;
 int outputValue = 0;
-
-const unsigned long requestInterval = 30*1000;    // delay between requests; 10 seconds
-
-boolean requested;                     // whether request wass made  since connecting
-unsigned long lastAttemptTime = 0;     // last time of connection to the server, in milliseconds
 
 String data = "";
 
 void setup() {
-  data.reserve(128024);
+
+  data.reserve(600);
   Serial.begin(9600);
   while(!Serial) {
     ; // Neede for Leonardo board only
   }
-  
+
   if (WiFi.status() == WL_NO_SHIELD) {
     Serial.println("WiFI shield not present");
     while(true);
   }
-  
+
   while(status != WL_CONNECTED) {
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(ssid);
-    
+
     status = WiFi.begin(ssid, pass);
-    
+
     delay(1000); // wait 10 seconds for reconnection
   }
-  
+
   Serial.print("You're connected to the network\n");
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
-  Serial.println(ip);
-  
-  connectToServer(); 
+  Serial.println(ip); 
 }
 
 void loop() {
@@ -66,27 +61,42 @@ void loop() {
     char c = client.read();
     Serial.write(c);
   }
+  collectData(); 
+}
 
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting from server.");
-   
-    
-  } else if (millis() - lastAttemptTime > requestInterval) {
-  
-    connectToServer();
+String collectData() {
+  int i;  
+  for(i=0; i<=600; i++ ) {
+    sensorValue = analogRead(analogInPin);
+    outputValue = map(sensorValue, 0, 1023, 0, 255);
+    analogWrite(analogOutPin, outputValue);
+    data += sensorValue;
+    data += ",";
+    Serial.println(sensorValue);
   }
+ 
+  sendData(data);
+  i=0;
 }
 
-void connectToServer() {
-  
+void sendData(String data) {
   if (client.connect(serverName, 80)) {
-    Serial.println("connected to server");
-    client.println("GET /api/device/666/ HTTP/1.1");
+    Serial.print("connected to server\n");
+    client.println("POST /api/device/stara/testmatkozak/ HTTP/1.1");
+    Serial.print("POST\n");
     client.println("Host:openlaundryapi.org");
-    client.println("Keep-Alive: 115");
-    client.println("Connection: keep-alive");
+    Serial.print("HOST\n");
+    client.print("Content-Length: ");
+    client.println(data.length());
+    Serial.println(data.length());
+    Serial.print("data length\n");
     client.println();
-   }
+    client.print(data);
+    Serial.print("data:\n");
+    Serial.print(data);
+    client.println();
+  } 
 }
+
+
+
